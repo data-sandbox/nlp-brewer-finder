5e74e5f0d8876dcd534f2b66c5c25001a619bb98
99933422+data-science-sandbox@users.noreply.github.com <99933422+data-science-sandbox@users.noreply.github.com>
2023-02-09 21:32:19 -0500

    Add real data. Add all initial filter checkboxes.

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -4,10 +4,10 @@ import pandas as pd
 import pydeck as pdk
 import requests
 
-st.set_page_config(layout='wide', page_title='Brewer Finder App')
+st.set_page_config(layout='wide', page_title='Brewery Finder App 🍻')
 
 # Title
-st.markdown('# Brewery Finder App')
+st.markdown('# Brewery Finder 🍻')
 st.write('Find nearby breweries that match your desired features.')
 
 
@@ -25,20 +25,14 @@ def geocode(address):
 
 
 def plot_map(df, lat=None, lon=None):
-    # Remove locations with null lat/lon values
-    df = df.dropna()
-
-    if lat is None:
-        view_state = pdk.data_utils.compute_view(
-            df[['longitude', 'latitude']], 0.8)
-    else:
-        # view_state = pdk.ViewState(
-        #     latitude=lat,
-        #     longitude=lon,
-        #     zoom=11,
-        #     pitch=0)
-        view_state = pdk.data_utils.compute_view(
-            df[['longitude', 'latitude']], 0.8)
+    # if lat is None:
+    #     view_state = pdk.data_utils.compute_view(
+    #         df[['longitude', 'latitude']], 0.8)
+    # else:
+    # view_state = pdk.ViewState(latitude=lat, longitude=lon,
+    #                            zoom=10, bearing=0, pitch=0)
+    view_state = pdk.data_utils.compute_view(
+        df[['longitude', 'latitude']], 0.8)
 
     st.pydeck_chart(pdk.Deck(
         map_style=None,
@@ -58,35 +52,46 @@ def plot_map(df, lat=None, lon=None):
     return
 
 
-def load_df(outdoors, dogs):
+def load_df():
     # raw df
-    filepath = Path('./assets/ma_breweries_features.csv')
+    filepath = Path('./assets/breweries_app.csv')
     df = pd.read_csv(filepath)
-    if outdoors and not dogs:
-        query_df = df.query('has_patio == 1')
-    elif dogs and not outdoors:
-        query_df = df.query('dog_friendly == 1')
-    elif outdoors and dogs:
-        query_df = df.query('has_patio == 1 & dog_friendly == 1')
-    else:
+    query = []
+    if outdoors:
+        query.append('outdoors == 1')
+    if food:
+        query.append('food == 1')
+    if dogs:
+        query.append('dogs == 1')
+    if music:
+        query.append('music == 1')
+    if games:
+        query.append('games == 1')
+    if tours:
+        query.append('tours == 1')
+    if not query:
         query_df = df
+    else:
+        query_df = df.query(" & ".join(query))
     return query_df
 
 
-def display_map(query_df, address=None):
-    if address:
-        lat, lon = geocode(address)
-    else:
-        lat = None
-        lon = None
+def display_map(query_df, lat, lon):
+    # if address:
+    #     lat, lon = geocode(address)
+    # else:
+    #     lat = None
+    #     lon = None
 
-    plot_map(query_df, lat, lon)
+    plot_map(query_df)
     return
 
 
 def display_df(query_df):
     # clean df for display
-    columns = ['name', 'city', 'street', 'has_food', 'has_patio']
+    columns = ['name', 'state', 'city', 'street',
+               #    'outdoors', 'food', 'games', 'music', 'tours'
+               ]
     display_df = query_df[columns].sort_values(by='city')
     return display_df
 
@@ -96,40 +101,29 @@ st.sidebar.subheader("""Find Nearby Breweries""")
 address_input = st.sidebar.text_input("Enter address:", "Boston, MA")
 find_button = st.sidebar.button("FIND")
 
-if find_button:
-    url = 'https://api.mapbox.com/geocoding/v5/{endpoint}/{search_text}.json'
-
 # Sidebar filters
 st.sidebar.subheader("""Filters""")
-outdoor_box = st.sidebar.checkbox("Outdoor Seating")
-dog_box = st.sidebar.checkbox("Dog Friendly")
-food_box = st.sidebar.checkbox("Food Available")
-tour_box = st.sidebar.checkbox("Tours Available")
-group_box = st.sidebar.checkbox("Good for Large Groups")
+outdoors = st.sidebar.checkbox("Outdoor Seating")
+food = st.sidebar.checkbox("Food Served")
+dogs = st.sidebar.checkbox("Dog Friendly")
+games = st.sidebar.checkbox("Games Available")
+music = st.sidebar.checkbox("Live Music")
+tours = st.sidebar.checkbox("Tours Offered")
 
-# defaults
-outdoors = False
-dogs = False
-address = None
-
-if outdoor_box:
-    outdoors = True
-if dog_box:
-    dogs = True
-if tour_box:
-    tours = True
 
 if find_button:
-    address = address_input
+    lat, lon = geocode(address_input)
+    st.write(lat, lon)
 
-df = load_df(outdoors, dogs)
-display_map(df, address)
+df = load_df()
+# display_map(df, lat, lon)
+plot_map(df)
 st.dataframe(display_df(df))
 
 st.subheader("About this app")
 st.markdown("""
 This app... <insert description>
 
-You can see how this works in the my [Jupyter Notebooks](https://github.com/data-science-sandbox/nlp-brewer-finder/tree/main/notebooks) 
+You can see how this works in the my [Jupyter Notebooks](https://github.com/data-science-sandbox/nlp-brewer-finder/tree/main/notebooks)
 or [see all the code](https://github.com/data-science-sandbox/nlp-brewer-finder).
 """)
